4e10055cdf752a8b2b71b5761419df4c193a1606
Felipe-Arballo <146143237+Felipe-Arballo@users.noreply.github.com>
2023-11-08 18:52:21 -0300

    Add files via upload

---
+++ Obj2_LOGARITMO.py
@@ -0,0 +1,67 @@
+# Objetivo 2: Cifrado Atbash
+# Tomamos como precondicion que la cadena sea un str
+# En el abecedario usamos la "ñ" y en los numeros empezamos desde el "0"
+
+def cifrado_atbash(cadena):
+    abecedario = "abcdefghijklmnñopqrstuvwxyz"
+    abecedario_invertido = abecedario[::-1]
+    numeros = "0123456789"
+    numeros_invertido = numeros[::-1]
+    cadena_codificada = ""
+    for letra in cadena:
+        if letra.isalpha():
+            letra_minus = letra.lower()
+            indice = abecedario.index(letra_minus)
+            if letra.isupper():
+                cadena_codificada += abecedario_invertido[indice]
+            else:
+                cadena_codificada += abecedario_invertido[indice].upper()
+        elif letra.isnumeric():
+            indice = numeros.index(letra)
+            cadena_codificada += numeros_invertido[indice]
+        else:
+            cadena_codificada += letra
+    return cadena_codificada
+
+# print(cifrado_atbash(""))
+
+'''
+Funcion hecha por:
+    Arballo Felipe Antonio
+    Rojas Bravo Diego Angel 
+    Saladino Joaquin
+'''      
+
+import doctest
+
+def pruebas2():
+    '''
+    >>> cifrado_atbash("HOLA MUNDO") # Mayusculas
+    'sloz ñfnwl'
+    >>> cifrado_atbash("hola mundo") # Minusculas
+    'SLOZ ÑFNWL'
+    >>> cifrado_atbash("0123456789: Num") # Números
+    '9876543210: nFÑ'
+    >>> cifrado_atbash("123 *#+-") # Simbolos
+    '876 *#+-'
+    >>> cifrado_atbash("test5")
+    'GVHG4'
+    >>> cifrado_atbash("TEST6")
+    'gvhg3'
+    >>> cifrado_atbash("Test :-)")
+    'gVHG :-)'
+    >>> cifrado_atbash("sloz ñfnwl") # Desencriptado
+    'HOLA MUNDO'
+    >>> cifrado_atbash(" ") # Cadena de espacio
+    ' '
+    >>> cifrado_atbash("") # Cadena vacia
+    ''
+    '''
+
+print(doctest.testmod())
+
+'''
+Funcion hecha por:
+    Mancco Puma Osk'r Fabricio
+    Maldonado Aluhe 
+'''
---
+++ TP1_LOGARITMO.py
@@ -0,0 +1,161 @@
+from tkinter import*
+
+# # Importamos tkinter y debajo estan los cifrados de los objetivos anterirores
+def cifrado_cesar(cadena, clave):
+    cadena_modificada = ""
+    indice = 0
+    while indice < len(cadena):
+        letra = cadena[indice]
+        if letra.isalnum():
+            letra_desplazada = ord(letra) + clave
+            cadena_modificada += chr(letra_desplazada)
+        else:
+            cadena_modificada += letra
+        indice += 1
+    return cadena_modificada
+
+def cifrado_atbash(cadena):
+    abecedario = "abcdefghijklmnñopqrstuvwxyz"
+    abecedario_invertido = abecedario[::-1]
+    numeros = "0123456789"
+    numeros_invertido = numeros[::-1]
+    cadena_codificada = ""
+    for letra in cadena:
+        if letra.isalpha():
+            letra_minuscula = letra.lower()
+            indice = abecedario.index(letra_minuscula)
+            if letra.isupper():
+                cadena_codificada += abecedario_invertido[indice]
+            else:
+                cadena_codificada += abecedario_invertido[indice].upper()
+        elif letra.isnumeric():
+            indice = numeros.index(letra)
+            cadena_codificada += numeros_invertido[indice]
+        else:
+            cadena_codificada += letra
+    return cadena_codificada
+
+def ventana_funcional():
+    # # Funciones intermedias que señalan errores y mandan los datos a los cifrados (4)
+    def CESAR_intermedio():
+        if type(entrada_clave.get()) != int:
+            resultado = "CLAVE INVALIDA"
+            devolver_valor(resultado, True)
+
+        clave = int(entrada_clave.get())
+        mensaje = entrada_mensaje.get()
+
+        if clave < 0:
+            resultado = "CLAVE INVALIDA"
+            devolver_valor(resultado, True)
+
+        elif not mensaje: 
+            resultado = "FALTA EL MENSAJE"
+            devolver_valor(resultado, True)
+
+        else:
+            resultado = cifrado_cesar(mensaje,clave)
+            devolver_valor(resultado, False)
+            
+    def des_CESAR_intermedio():
+        if type(entrada_clave.get()) != int:
+            resultado = "CLAVE INVALIDA"
+            devolver_valor(resultado, True)
+
+        clave = -int(entrada_clave.get())
+        mensaje = entrada_mensaje.get()
+
+        if clave > 0:
+            resultado = "CLAVE INVALIDA"
+            devolver_valor(resultado, True)
+
+        elif not mensaje: 
+            resultado = "FALTA EL MENSAJE"
+            devolver_valor(resultado, True)
+            
+        else:
+            resultado = cifrado_cesar(mensaje,clave)
+            devolver_valor(resultado, False)
+    
+    def ATBASH_intermedio():
+        mensaje = entrada_mensaje.get()
+        if not mensaje:
+            resultado = "FALTA EL MENSAJE"
+            devolver_valor(resultado, True)
+        else:
+            resultado = cifrado_atbash(mensaje)
+            devolver_valor(resultado, False)
+    
+    # # Creamos la segunda ventana (2)
+    raiz1.destroy()
+    raiz2 = Tk()
+    raiz2.title("TP Grupal Parte 1 - Grupo: LOGARITMO")
+    raiz2.geometry("700x450")
+    raiz2.resizable(0,0)
+    raiz2.iconbitmap(r"C:\Users\Usuario\OneDrive\Escritorio\Felipe\Algoritmos y Programación\TP 1\icono.ico")
+    
+    texto_mensaje = Label(raiz2 , text = "Ingrese el mensaje a cifrar: ")
+    texto_mensaje.config(padx=10 , pady=10 , font=("Arial" , 12))
+    
+    entrada_mensaje = Entry(raiz2)
+    entrada_mensaje.config(bg="pink")
+    entrada_mensaje.place(width=200 , height=40)
+    
+    texto_clave = Label(raiz2 , text = "Ingrese la clave del cifrado CESAR: ")
+    texto_clave.config(padx=10 , pady=10 , font=("Arial" , 12))
+    
+    entrada_clave = Entry(raiz2)
+    entrada_clave.config(bg="pink")
+    entrada_clave.place(width=200 , height=40)
+    
+    # Ponemos los botones de cifrado y decifrado (3)
+    cifrado_CESAR = Button(raiz2 , text="Cifrado CESAR" , bd=10 , relief="ridge" , cursor="hand2" , command=CESAR_intermedio)
+    
+    cifrado_ATBASH = Button(raiz2 , text="Cifrado ATBASH" , bd=10 , relief="ridge" , cursor="hand2" , command=ATBASH_intermedio)
+    
+    descifrado_CESAR = Button(raiz2 , text="Descifrado CESAR" , bd=10 , relief="ridge" , cursor="hand2" , command=des_CESAR_intermedio)
+    
+    descifrado_ATBASH = Button(raiz2 , text="Descifrado ATBASH" , bd=10 , relief="ridge" , cursor="hand2" , command=ATBASH_intermedio)
+    
+    resultado_final = Label(raiz2 , text=" ")
+    
+    # Funcion que muestra los errores o el resultado (5)
+    def devolver_valor(resultado, error):
+        if not error:
+            resultado_final.config(bg="grey" , bd=5 , relief="raised" , text="Resultado: " + resultado , width=50 , height=50 , font=("Arial" , 15))
+        else:
+            resultado_final.config(bg="red" , bd=5 , relief="raised" , text= resultado , width=50 , height=50 , font=("Arial" , 15))
+    
+    texto_mensaje.pack()
+    entrada_mensaje.pack()
+    texto_clave.pack()
+    entrada_clave.pack()
+    cifrado_CESAR.pack(padx=20 , pady=15)
+    cifrado_ATBASH.pack(padx=20 , pady=15)
+    descifrado_CESAR.pack(padx=20 , pady=15)
+    descifrado_ATBASH.pack(padx=20 , pady=15)
+    resultado_final.pack(padx=20 , pady=0)
+
+# # Creamos la primer ventana (1)
+
+raiz1 = Tk()
+raiz1.title("TP Grupal Parte 1 - Grupo: LOGARITMO")
+raiz1.geometry("700x450")
+raiz1.resizable(0,0)
+raiz1.iconbitmap(r"C:\Users\Usuario\OneDrive\Escritorio\Felipe\Algoritmos y Programación\TP 1\icono.ico")
+
+bienvenida = Label(raiz1 , text="Bienvenido a la aplicación de mensajes secretos del grupo LOGARITMO. \n Para continuar presione continuar, de lo contrario cierre la ventana.")
+bienvenida.config(font=("Calibri" , 15) )
+
+continuar = Button(raiz1 , text = "CONTINUAR" , command = ventana_funcional) 
+continuar.config(bg="red" , bd=10 , relief="raised" , font=("Arial" , 15))
+continuar.config(cursor="hand2")
+
+integrantes = Label(raiz1 , text=" \n Construída por: \n - Arballo Felipe Antonio \n - Maldonado Aluhe Nahuel \n - Mancco Puma Osk'r Fabricio \n - Rojas Bravo Diego Ángel \n - Saladino Joaquín")
+integrantes.config(font=("Calibri" , 12))
+
+bienvenida.pack(pady=50)
+continuar.pack(padx=150 , pady=0)
+integrantes.pack(pady=10)
+
+raiz1.mainloop()
