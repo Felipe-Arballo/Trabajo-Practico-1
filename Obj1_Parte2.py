e13d932b46e9411af0b3f4d426767bd9a24decb7
Felipe-Arballo <146143237+Felipe-Arballo@users.noreply.github.com>
2023-11-27 15:55:09 -0300

    Update Obj1_Parte2.py

--- Obj1_Parte2.py
+++ Obj1_Parte2.py
@@ -1,31 +1,3 @@
-'''
-1. Creación de Usuario
-El “Crear Usuario”, deberá desplegar una nueva ventana para poder registrar un usuario, su clave y una
-pregunta de seguridad para recuperación de la clave.
-El identificador del usuario debe ser validado, sólo puede estar formado por letras, números, y los
-caracteres “_” “-” “.”; y tener como mínimo 5 caracteres y como máximo 15. Escribir una función de
-validación del identificador del usuario, e incluir al menos 10 casos de prueba significativos.
-
-A su vez, la clave del usuario, deberá tener una longitud de entre 4 y 8 caracteres, formada por al menos
-una letra mayúscula, una letra minúscula, un número, y al menos uno de los siguientes caracteres: “_”
-“-” “#” “*”. Además, no puede haber caracteres repetidos adyacentes. Escribir una función de
-validación de la clave del usuario, e incluir al menos 10 casos de prueba significativos.
-
-La pregunta para recuperación de clave debe poder ser elegida entre las siguientes opciones, que
-deberán estar en el archivo preguntas.csv:
-1,Apellido de su abuela materna
-2,Nombre de tu mascota
-3,Nombre de tu mejor amigo/amiga
-4,Cantante preferido
-5,Ciudad preferida
-Deben agregar 5 preguntas más a las existentes al archivo, cada una de las cuales debe estar identificada
-por los números del 6 al 10 respectivamente.
-Los datos de los usuarios creados deben ser guardados en un archivo csv, cuyo formato debe ser:
-Id_usuario,clave_usuario,id_pregunta,respuesta_recuperacion,intentos_recuperacion
-En la creación del usuario se debe controlar que el identificador del usuario no exista previamente en el
-archivo, en caso de existir, se debe informar “Identificador en uso”
-'''
-
 from tkinter import *
 
 def validar_nombre(nombre):
