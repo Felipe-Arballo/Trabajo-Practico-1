ae35b68434a6eab8dae731a9d174150a649d211c
Felipe-Arballo <146143237+Felipe-Arballo@users.noreply.github.com>
2023-11-29 13:30:32 -0300

    Update Funciones_Validar.py

--- Funciones_Validar.py
+++ Funciones_Validar.py
@@ -1,8 +1,6 @@
 from tkinter import *
-from Funciones_Cifrados import (cifrado_atbash , cifrado_cesar , devolver_valor)
+from Funciones_Cifrados import cifrado_atbash, cifrado_cesar
 
-abecedario = "abcdefghijklmnñopqrstuvwxyz"
-numeros = "0123456789"
 clave_no_numero = "CLAVE NO NÚMERO"
 clave_invalida = "CLAVE INVÁLIDA"
 falta_mensaje = "FALTA EL MENSAJE"
@@ -56,43 +54,72 @@ def validar_clave(clave):
         resultado_final = False
     return resultado_final
 
-def validar_cesar(entrada_clave, entrada_mensaje):
+def validar_cesar(raiz2, entrada_clave, entrada_mensaje):
     if not entrada_clave.get().isnumeric():
-        resultado = clave_no_numero
-        devolver_valor(resultado, True)
-    clave = int(entrada_clave.get())
-    mensaje = entrada_mensaje.get()
-    if clave < 0:
-        resultado = clave_invalida
-        devolver_valor(resultado, True)
-    elif not mensaje: 
-        resultado = falta_mensaje
-        devolver_valor(resultado, True)
-    else:
-        resultado = cifrado_cesar(mensaje,clave)
-        devolver_valor(resultado, False)
-
-def validar_cesar2(entrada_clave, entrada_mensaje): 
-    if not entrada_clave.get().isnumeric():
-        resultado = clave_no_numero
-        devolver_valor(resultado, True)
+        mensaje_descifrado = clave_no_numero
+        resultado = False
     clave = -int(entrada_clave.get())
     mensaje = entrada_mensaje.get()
     if clave > 0:
-        resultado = clave_invalida
-        devolver_valor(resultado, True)
+        mensaje_descifrado = clave_invalida
+        resultado = False
     elif not mensaje: 
-        resultado = falta_mensaje
-        devolver_valor(resultado, True)
+        mensaje_descifrado = falta_mensaje
+        resultado = False
     else:
-        resultado = cifrado_cesar(mensaje,clave)
-        devolver_valor(resultado, False)
+        mensaje_descifrado = cifrado_cesar(mensaje,int(clave))
+        resultado = True
+    devolver_valor(mensaje_descifrado, resultado, raiz2)
+    return resultado
 
-def validar_atbash(entrada_mensaje):
+def validar_cesar2(raiz2, entrada_clave, entrada_mensaje): 
+    if not entrada_clave.get().isnumeric():
+        mensaje_descifrado = clave_no_numero
+        resultado = False
+    clave = -int(entrada_clave.get())
+    mensaje = entrada_mensaje.get()
+    if clave > 0:
+        mensaje_descifrado = clave_invalida
+        resultado = False
+    elif not mensaje: 
+        mensaje_descifrado = falta_mensaje
+        resultado = False
+    else:
+        mensaje_descifrado = cifrado_cesar(mensaje,-int(clave))
+        resultado = True
+    devolver_valor(mensaje_descifrado, resultado, raiz2)
+    return resultado
+
+def validar_atbash(raiz2, entrada_mensaje):
     mensaje = entrada_mensaje.get()
     if not mensaje:
-        resultado = falta_mensaje
-        devolver_valor(resultado, True)
+        mensaje_descifrado = falta_mensaje
+        resultado = False
     else:
-        resultado = cifrado_atbash(mensaje)
-        devolver_valor(resultado, False)
+        mensaje_descifrado = cifrado_atbash(mensaje)
+        resultado = True
+    devolver_valor(mensaje_descifrado, resultado,raiz2)
+    return resultado
+
+def devolver_valor(resultado, validacion, raiz2):
+    parametros = definir_parametros_resultado()
+    resultado_final = Label(raiz2 , name = "#")
+    if validacion:
+            resultado_final.config(bg=parametros["resultado_config"][1] , bd=parametros["resultado_config"][2] , relief=parametros["resultado_config"][3] , text=parametros["resultado_config"][4] + resultado , width=parametros["resultado_config"][5] , height=parametros["resultado_config"][6] , font=(parametros["resultado_config"][7] , parametros["resultado_config"][8]))
+    else:
+        resultado_final.config(bg=parametros["resultado_config"][0] , bd=parametros["resultado_config"][2] , relief=parametros["resultado_config"][3] , text= resultado , width=parametros["resultado_config"][5] , height=parametros["resultado_config"][6] , font=(parametros["resultado_config"][7] , parametros["resultado_config"][8]))
+    resultado_final.pack()
+
+def definir_parametros_resultado():
+    parametros = {}
+    color_error = "red"
+    color_resultado = "grey"
+    borde = 5
+    relieve = "raised"
+    texto = "Resultado:"
+    width = 50
+    height = 50
+    tipo_letra = "Arial"
+    tamaño_letra = 15
+    parametros["resultado_config"] =(color_error, color_resultado, borde, relieve, texto, width, height, tipo_letra, tamaño_letra)
+    return parametros
