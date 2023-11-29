6db6bacce6c1047b450323121a564f96096fe6fb
JoaquinSaladino <111785161+JoaquinSaladino@users.noreply.github.com>
2023-11-29 16:33:17 -0300

    Update TP2_LOGARITMO.py

--- TP2_LOGARITMO.py
+++ TP2_LOGARITMO.py
@@ -8,15 +8,15 @@ def crear_ventana_principal(parametros):
     raiz1.title(parametros["ventana_principal"][0])
     raiz1.geometry(parametros["ventana_principal"][1])
     raiz1.resizable(0,0)
-    raiz1.iconbitmap("icono.ico")
+    raiz1.iconbitmap(parametros["ventana_principal"][2])
 
     bienvenida = Label(raiz1 , text=parametros["bienvenida_config"][0])
     bienvenida.config(font=(parametros["bienvenida_config"][1], parametros["bienvenida_config"][2]) )
     
-    crear_boton = Button(raiz1 , text="Crear Usuario" , command = lambda: creo_usuario(raiz1))
+    crear_boton = Button(raiz1 , text=parametros["continuar_config"][6] , command = lambda: creo_usuario(raiz1))
     crear_boton.config(bg=parametros["continuar_config"][0] , bd=parametros["continuar_config"][1], relief=parametros["continuar_config"][2], cursor=parametros["continuar_config"][3], font=((parametros["continuar_config"][4]), parametros["continuar_config"][5]))
 
-    ingresar_boton = Button(raiz1 , text="Ingresar Usuario" , command = lambda: ingreso_usuario(raiz1))
+    ingresar_boton = Button(raiz1 , text=parametros["continuar_config"][7] , command = lambda: ingreso_usuario(raiz1))
     ingresar_boton.config(bg=parametros["continuar_config"][0] , bd=parametros["continuar_config"][1], relief=parametros["continuar_config"][2], cursor=parametros["continuar_config"][3], font=((parametros["continuar_config"][4]), parametros["continuar_config"][5]))
 
     integrantes = Label(raiz1 , text=parametros["integrantes_config"][0])
@@ -34,7 +34,8 @@ def definir_parametros():
 
     titulo = "TP Grupal Parte 1 - Grupo: LOGARITMO"
     tamaño = "700x450"
-    parametros["ventana_principal"] = (titulo , tamaño)
+    icono = "icono.ico"
+    parametros["ventana_principal"] = (titulo , tamaño , icono)
 
     texto_bienvenida = "Bienvenido a la aplicación de mensajes secretos del grupo LOGARITMO. \n Para continuar presione continuar, de lo contrario cierre la ventana."
     tipo_letra_bienvenida = "Calibri"
@@ -48,7 +49,9 @@ def definir_parametros():
     tipo_letra_continuar = "Arial"
     tamaño_letra_continuar = 15
     nombre_boton = "CONTINUAR"
-    parametros["continuar_config"] = (color_continuar, borde_continuar, tipo_borde_continuar, tipo_cursor, tipo_letra_continuar, tamaño_letra_continuar, nombre_boton)
+    texto_crear = "Crear Usuario"
+    texto_ingresar = "Ingresar Usuario"
+    parametros["continuar_config"] = (color_continuar, borde_continuar, tipo_borde_continuar, tipo_cursor, tipo_letra_continuar, tamaño_letra_continuar, nombre_boton, texto_crear, texto_ingresar)
 
     texto_integrantes = " \n Construída por: \n - Arballo Felipe Antonio \n - Maldonado Aluhe Nahuel \n - Mancco Puma Osk'r Fabricio \n - Rojas Bravo Diego Ángel \n - Saladino Joaquín"
     tipo_letra_integrantes = "Calibri"
@@ -56,7 +59,6 @@ def definir_parametros():
     parametros["integrantes_config"] = (texto_integrantes, tipo_letra_integrantes, tamaño_letra_integrantes)
 
     return parametros
-
 
 def main():
     crear_ventana_principal(definir_parametros())
