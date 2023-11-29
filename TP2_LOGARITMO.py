d0722016d9b4dc8fda8934dd8953f5ec74839cf2
JoaquinSaladino <111785161+JoaquinSaladino@users.noreply.github.com>
2023-11-29 16:42:31 -0300

    Update TP2_LOGARITMO.py

--- TP2_LOGARITMO.py
+++ TP2_LOGARITMO.py
@@ -11,7 +11,7 @@ def crear_ventana_principal(parametros):
     raiz1.iconbitmap(parametros["ventana_principal"][2])
 
     bienvenida = Label(raiz1 , text=parametros["bienvenida_config"][0])
-    bienvenida.config(font=(parametros["bienvenida_config"][1], parametros["bienvenida_config"][2]) )
+    bienvenida.config(font=(parametros["bienvenida_config"][1], parametros["bienvenida_config"][2]))
     
     crear_boton = Button(raiz1 , text=parametros["continuar_config"][6] , command = lambda: creo_usuario(raiz1))
     crear_boton.config(bg=parametros["continuar_config"][0] , bd=parametros["continuar_config"][1], relief=parametros["continuar_config"][2], cursor=parametros["continuar_config"][3], font=((parametros["continuar_config"][4]), parametros["continuar_config"][5]))
@@ -48,10 +48,9 @@ def definir_parametros():
     tipo_cursor = "hand2"
     tipo_letra_continuar = "Arial"
     tamaño_letra_continuar = 15
-    nombre_boton = "CONTINUAR"
     texto_crear = "Crear Usuario"
     texto_ingresar = "Ingresar Usuario"
-    parametros["continuar_config"] = (color_continuar, borde_continuar, tipo_borde_continuar, tipo_cursor, tipo_letra_continuar, tamaño_letra_continuar, nombre_boton, texto_crear, texto_ingresar)
+    parametros["continuar_config"] = (color_continuar, borde_continuar, tipo_borde_continuar, tipo_cursor, tipo_letra_continuar, tamaño_letra_continuar, texto_crear, texto_ingresar)
 
     texto_integrantes = " \n Construída por: \n - Arballo Felipe Antonio \n - Maldonado Aluhe Nahuel \n - Mancco Puma Osk'r Fabricio \n - Rojas Bravo Diego Ángel \n - Saladino Joaquín"
     tipo_letra_integrantes = "Calibri"
